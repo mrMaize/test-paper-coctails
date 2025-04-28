0909717b332d54af68224bd097d4c4645734fc27
Artem Kukuruzza <artem.kuka@gmail.com>
2025-04-28 16:22:50 +0300

    removed proxy

--- src/shared/api/apiMethods.ts
+++ src/shared/api/apiMethods.ts
@@ -1,5 +1,7 @@
+const BASE_URL = 'https://www.thecocktaildb.com';
+
 export const get = async <T = unknown>(url: string): Promise<T> => {
-  return fetch(url)
+  return fetch(BASE_URL + url)
     .then((result) => result.json())
     .catch((error) => {
       console.error(error);
